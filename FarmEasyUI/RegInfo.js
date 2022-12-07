826d80e5c35daddfc92d05201b1a4c82769dd437
mdasif22 <imd.asif222@gmail.com>
2022-12-07 17:37:05 +0530

    "adding"

--- FarmEasyUI/RegInfo.js
+++ FarmEasyUI/RegInfo.js
@@ -153,47 +153,47 @@ function regasbuyer () {
   })
 }
 
-  //Set up our login function
-  function login () {
-    // Get all our input fields
-    email = document.getElementById('email').value
-    password = document.getElementById('password').value
+  // //Set up our login function
+  // function login () {
+  //   // Get all our input fields
+  //   email = document.getElementById('email').value
+  //   password = document.getElementById('password').value
   
-    // Validate input fields
-    if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password is Outta Line!!')
-      return
-      // Don't continue running the code
-    }
+  //   // Validate input fields
+  //   if (validate_email(email) == false || validate_password(password) == false) {
+  //     alert('Email or Password is Outta Line!!')
+  //     return
+  //     // Don't continue running the code
+  //   }
   
-    auth.signInWithEmailAndPassword(email, password)
-    .then(function() {
-      // Declare user variable
-      var user = auth.currentUser
+  //   auth.signInWithEmailAndPassword(email, password)
+  //   .then(function() {
+  //     // Declare user variable
+  //     var user = auth.currentUser
   
-      // Add this user to Firebase Database
-      var database_ref = database.ref()
+  //     // Add this user to Firebase Database
+  //     var database_ref = database.ref()
   
-      // Create User data
-      var user_data = {
-        last_login : Date.now()
-      }
+  //     // Create User data
+  //     var user_data = {
+  //       last_login : Date.now()
+  //     }
   
-      // Push to Firebase Database
-      database_ref.child('users/' + user.uid).update(user_data)
+  //     // Push to Firebase Database
+  //     database_ref.child('users/' + user.uid).update(user_data)
   
-      // DOne
-      alert('User Logged In!!')
+  //     // DOne
+  //     alert('User Logged In!!')
   
-    })
-    .catch(function(error) {
-      // Firebase will use this to alert of its errors
-      var error_code = error.code
-      var error_message = error.message
+  //   })
+  //   .catch(function(error) {
+  //     // Firebase will use this to alert of its errors
+  //     var error_code = error.code
+  //     var error_message = error.message
   
-      alert(error_message)
-    })
-  }
+  //     alert(error_message)
+  //   })
+  // }
   
   
   
