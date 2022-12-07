b628351666bd64d3a98e0118be006d5b4960dd42
mdasif22 <imd.asif222@gmail.com>
2022-12-07 22:53:11 +0530

    "database"

--- FarmEasyUI/RegInfo.js
+++ FarmEasyUI/RegInfo.js
@@ -154,48 +154,88 @@ function regasbuyer () {
 }
 
   // //Set up our login function
-  // function login () {
-  //   // Get all our input fields
-  //   email = document.getElementById('email').value
-  //   password = document.getElementById('password').value
+  function logfarmer () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
   
-  //   // Validate input fields
-  //   if (validate_email(email) == false || validate_password(password) == false) {
-  //     alert('Email or Password is Outta Line!!')
-  //     return
-  //     // Don't continue running the code
-  //   }
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
   
-  //   auth.signInWithEmailAndPassword(email, password)
-  //   .then(function() {
-  //     // Declare user variable
-  //     var user = auth.currentUser
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
   
-  //     // Add this user to Firebase Database
-  //     var database_ref = database.ref()
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
   
-  //     // Create User data
-  //     var user_data = {
-  //       last_login : Date.now()
-  //     }
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
   
-  //     // Push to Firebase Database
-  //     database_ref.child('users/' + user.uid).update(user_data)
+      // Push to Firebase Database
+      database_ref.child('farmer/' + user.uid).update(user_data)
   
-  //     // DOne
-  //     alert('User Logged In!!')
+      // DOne
+      alert('Farmer Logged In!!')
   
-  //   })
-  //   .catch(function(error) {
-  //     // Firebase will use this to alert of its errors
-  //     var error_code = error.code
-  //     var error_message = error.message
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
   
-  //     alert(error_message)
-  //   })
-  // }
+      alert(error_message)
+    })
+  }
   
+  // //Set up our login function
+  function logbuyer () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
   
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is Outta Line!!')
+      return
+      // Don't continue running the code
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('buyer/' + user.uid).update(user_data)
+  
+      // DOne
+      alert('Buyer Logged In!!')
+  
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
   
   
   // Validate Functions
